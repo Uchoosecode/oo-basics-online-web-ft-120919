b7e8199caa112b45293abd20304e62b19f21a35e
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-19 17:26:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,15 @@
 
 
 class Shoe
-
+  attr_accessor :color
   def initialize(brand= "Addias")
     
     @brand = brand
   end
-  
-  
-  
+ 
   def brand
    @brand
   end
   
-end
-
+  
 Shoe.new("Nike")
